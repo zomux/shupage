c1a52d101fb1aeabb98431331afe05b593cad616
Raphael Shu <shu@shu-mbp.local>
2018-04-10 09:21:31 +0900

    -

--- node_modules/hexo-generator-topindex/index.js
+++ node_modules/hexo-generator-topindex/index.js
@@ -4,9 +4,9 @@
 
 var assign = require('object-assign');
 
-hexo.config.index_generator = assign({
+hexo.config.topindex_generator = assign({
   per_page: typeof hexo.config.per_page === 'undefined' ? 10 : hexo.config.per_page,
   order_by: '-date'
-}, hexo.config.index_generator);
+}, hexo.config.topindex_generator);
 
-hexo.extend.generator.register('index', require('./lib/generator'));
+hexo.extend.generator.register('topindex', require('./lib/generator'));
